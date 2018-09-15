b73bc3eaff70d3a5685ed0930de1e511cb3c3f55
alfonso-distiller <alfonso.delrio@live.com.mx>
2018-09-15 14:56:48 -0500

    Subject 38

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,23 @@ import React, {Component} from 'react';
 import Person from './person/person.jsx';
 
 export default class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 30},
+      {name: 'Manu', age: 13},
+      {name: 'Mario', age: 15}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="11">My hobbies are Racing</Person>
-        <Person name="Mario" age="13" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }

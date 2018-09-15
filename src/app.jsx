f5b4eefc811ba101c9abdf0d07223ad9a24c8a4d
alfonso-distiller <alfonso.delrio@live.com.mx>
2018-09-15 15:14:42 -0500

    Subject 39

--- src/app.jsx
+++ src/app.jsx
@@ -10,12 +10,16 @@ export default class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log('was clicked!')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>

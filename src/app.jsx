892db10d97bb4a030c5314f24996eb0d34cb938b
alfonso-distiller <alfonso.delrio@live.com.mx>
2018-09-16 10:49:19 -0500

    Section 53

--- src/app.jsx
+++ src/app.jsx
@@ -1,80 +1,70 @@
-import React, {Component} from 'react';
-import Person from './person/person.jsx';
+import React, { Component } from "react";
+import Person from "./person/person.jsx";
 
 export default class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age: 30},
-      {name: 'Manu', age: 13},
-      {name: 'Mario', age: 15}
+      { name: "Max", age: 30 },
+      { name: "Manu", age: 13 },
+      { name: "Mario", age: 15 }
     ],
     showPersons: false
-  }
+  };
 
-  switchNameHandler = (newName) => {
+  switchNameHandler = newName => {
     //console.log('was clicked!');
     // DONT DO THIS : this.state.persons[0].name = 'Maximus';
     this.setState({
       persons: [
-        {name: newName, age: 30},
-        {name: 'Manu', age: 13},
-        {name: 'Mario', age: 27}
+        { name: newName, age: 30 },
+        { name: "Manu", age: 13 },
+        { name: "Mario", age: 27 }
       ]
-    })
-  }
+    });
+  };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = event => {
     this.setState({
       persons: [
-        {name: 'Max', age: 30},
-        {name: event.target.value, age: 13},
-        {name: 'Mario', age: 27}
+        { name: "Max", age: 30 },
+        { name: event.target.value, age: 13 },
+        { name: "Mario", age: 27 }
       ]
-    })
-  }
+    });
+  };
 
   togglePersonsHandler = () => {
     this.setState({
       showPersons: !this.state.showPersons
-    })
-  }
+    });
+  };
 
   render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
     };
 
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = (<div>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}/>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Maximiliano')}
-          changed={this.nameChangedHandler}>
-          My hobbies are: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}/>
-      </div>);
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
+        </div>
+      );
     }
 
     return (
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>
+        <button style={style} onClick={this.togglePersonsHandler}>
           Switch Name
         </button>
         {persons}
--- src/person/person.jsx
+++ src/person/person.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './person.css';
+import "./person.css";
 
-const person = (props) => {
+const person = props => {
   return (
     <div className="person">
-      <p onClick={props.click}>Im {props.name} and I am {props.age} years old!</p>
+      <p onClick={props.click}>
+        Im {props.name} and I am {props.age} years old!
+      </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };

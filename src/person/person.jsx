2d2c088676ffa8bee0bd0d449b6b3f8367390f9c
Alfonso del Río Juárez <alfonso.delrio@live.com.mx>
2018-09-19 12:19:49 -0500

    Section 66d

--- src/person/person.jsx
+++ src/person/person.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-
-import "./person.css";
+import classes from './person.css';
 
 const person = props => {
   return (
-    <div className="person">
+    <div className={classes.person}>
       <p onClick={props.click}>
-        Im {props.name} and I am {props.age} years old!
+        Im {props.name} and I am {props.age} years old !
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
